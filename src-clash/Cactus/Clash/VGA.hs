b41d05a29df02471d8a50cc74ed38303149bb1cc
Dr. ERDI Gergo <gergo@erdi.hu>
2019-02-09 17:36:15 +0800

    Change VGA module to use period length (in ps) in modespecs

--- src-clash/Cactus/Clash/VGA.hs
+++ src-clash/Cactus/Clash/VGA.hs
@@ -1,9 +1,11 @@
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE NumericUnderscores #-}
 module Cactus.Clash.VGA where
 
 import Clash.Prelude
 import Cactus.Clash.Util
+import Cactus.Clash.Clock
 import Data.Word
 
 data VGATiming n = VGATiming
@@ -28,8 +30,8 @@ data VGADriver dom w h = VGADriver
 
 vgaDriver
     :: (HiddenClockReset dom gated synchronous, KnownNat w, KnownNat h)
-    => (dom ~ Dom s ps, ps ~ FromHz rate)
-    => VGATimings rate w h
+    => (dom ~ Dom s ps)
+    => VGATimings ps w h
     -> VGADriver dom w h
 vgaDriver VGATimings{..} = VGADriver{..}
   where
@@ -56,28 +58,28 @@ vgaDriver VGATimings{..} = VGADriver{..}
     vMax = sum [vSize, vPre, vSync, vPost] - 1
 
 -- | VGA 640*480@60Hz, 25.175 MHz pixel clock
-vga640x480at60 :: VGATimings 25175000 10 10
+vga640x480at60 :: VGATimings (FromHz 25_175_000) 10 10
 vga640x480at60 = VGATimings
     { vgaHorizTiming = VGATiming 640 16 96 48
     , vgaVertTiming  = VGATiming 480 11  2 31
     }
 
 -- | VGA 800x600@72Hz, 50 MHz pixel clock
-vga800x600at72 :: VGATimings 50000000 11 10
+vga800x600at72 :: VGATimings (FromHz 50_000_000) 11 10
 vga800x600at72 = VGATimings
     { vgaHorizTiming = VGATiming 800 56 120 64
     , vgaVertTiming  = VGATiming 600 37   6 23
     }
 
 -- | VGA 800x600@60Hz, 40 MHz pixel clock
-vga800x600at60 :: VGATimings 40000000 11 10
+vga800x600at60 :: VGATimings (FromHz 40_000_000) 11 10
 vga800x600at60 = VGATimings
     { vgaHorizTiming = VGATiming 800 40 128 88
     , vgaVertTiming  = VGATiming 600  1   4 23
     }
 
 -- | VGA 1024*768@60Hz, 65 MHz pixel clock
-vga1024x768at60 :: VGATimings 65000000 11 10
+vga1024x768at60 :: VGATimings (FromHz 65_000_000) 11 10
 vga1024x768at60 = VGATimings
     { vgaHorizTiming = VGATiming 1024 24 136 160
     , vgaVertTiming  = VGATiming  768  3   6  29
