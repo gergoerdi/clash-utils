096d7a09df18fcb2cd19d55aa364c37d4fdd2252
Dr. ERDI Gergo <gergo@erdi.hu>
2019-03-09 10:09:02 +0800

    Type-directed product-of-sums ripple counting for VGA sync signal generation

---
+++ src-clash/Cactus/Clash/Counters.hs
@@ -0,0 +1,92 @@
+{-# LANGUAGE DataKinds, PolyKinds #-}
+module Cactus.Clash.Counters (counterPlus', counterMul) where
+
+import Clash.Prelude
+import Cactus.Clash.Util
+import Cactus.Clash.Product
+import Data.Word
+import Data.Proxy
+import Control.Monad.State
+
+import Data.Maybe
+import Control.Applicative
+
+import Data.Singletons -- ((~>))
+import Data.Singletons.Prelude.List (Map)
+
+counter
+    :: (HiddenClockReset dom gated synchronous, KnownNat n)
+    => proxy n
+    -> Signal dom Bool
+    -> Signal dom Bool
+    -> (Signal dom (Maybe (Index n)), Signal dom Bool)
+counter _ en tick = unbundle $ mealyState step Nothing $ bundle (en, tick)
+  where
+    step (en, tick) = do
+        when en $ modify $ \i ->
+            maybe (0 <$ guard tick) succIdx i
+        i' <- get
+        return (i', i' == Just maxBound)
+
+class IsCounterPlus ns where
+    type CounterSum ns (dom :: Domain) :: [*]
+
+    counterPlus'
+        :: (HiddenClockReset dom gated synchronous)
+        => proxy ns
+        -> Bool
+        -> Signal dom Bool
+        -> Signal dom Bool
+        -> (Product (CounterSum ns dom), Signal dom Bool)
+
+instance IsCounterPlus '[] where
+    type CounterSum '[] dom = dom ::: '[]
+
+    counterPlus' _ _ en tick = (NilP, tick)
+
+instance (KnownNat n, IsCounterPlus ns) => IsCounterPlus (n:ns) where
+    type CounterSum (n:ns) dom = Signal dom (Maybe (Index n)) : CounterSum ns dom
+
+    counterPlus' _ isFirst en tick = (this ::> that, end)
+      where
+        (this, next) = counter (Proxy @n) en (register isFirst tick)
+        (that, end) = counterPlus' (Proxy @ns) False en next
+
+counterPlus
+    :: (HiddenClockReset dom gated synchronous, IsCounterPlus ns)
+    => proxy ns
+    -> (Product (CounterSum ns dom), Signal dom Bool)
+counterPlus ns = (counters, end)
+  where
+    (counters, end) = counterPlus' ns True (pure True) end
+
+class IsCounterProd (nss :: [[Nat]]) where
+    type CounterProd nss (dom :: Domain) :: [[*]]
+
+    counterMul'
+        :: (HiddenClockReset dom gated synchronous)
+        => proxy nss
+        -> Signal dom Bool
+        -> Product (Map Product_ (CounterProd nss dom))
+
+instance IsCounterProd '[] where
+    type CounterProd '[] dom = dom ::: '[]
+
+    counterMul' _ _ = NilP
+
+instance (IsCounterPlus ns, IsCounterProd nss) => IsCounterProd (ns : nss) where
+    type CounterProd (ns : nss) dom = CounterSum ns dom : CounterProd nss dom
+
+    counterMul' _ prev = this ::> them
+      where
+        (this, endThis) = counterPlus' (Proxy @ns) True prev endThis
+        them = counterMul' (Proxy @nss) (register True endThis)
+
+data Product_ :: [*] ~> *
+type instance Apply Product_ ts = Product ts
+
+counterMul
+    :: (HiddenClockReset dom gated synchronous, IsCounterProd nss)
+    => proxy nss
+    -> Product (Map Product_ (CounterProd nss dom))
+counterMul nss = counterMul' nss (pure True)
---
+++ src-clash/Cactus/Clash/Product.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE DataKinds, PolyKinds #-}
+{-# LANGUAGE TypeFamilyDependencies, FlexibleInstances #-}
+{-# LANGUAGE DeriveGeneric, DeriveAnyClass, StandaloneDeriving #-}
+module Cactus.Clash.Product where
+
+import Clash.Prelude
+import GHC.Generics (Generic)
+import Control.DeepSeq
+
+class IsProduct (ts :: [*]) where
+    data Product ts
+    type UnbundledP (dom :: Domain) ts = (res :: [*]) | res -> dom ts
+
+    bundleP :: Product (UnbundledP dom ts) -> Signal dom (Product ts)
+    unbundleP :: Signal dom (Product ts) -> Product (UnbundledP dom ts)
+
+instance IsProduct '[] where
+    data Product '[] = NilP
+        deriving (Generic, NFData, Show, Undefined)
+    type UnbundledP dom '[] = dom ::: '[]
+
+    bundleP NilP = pure NilP
+    unbundleP _ = NilP
+
+infixr 5 ::>
+
+instance (IsProduct ts) => IsProduct (t : ts) where
+    data Product (t : ts) = t ::> (Product ts)
+
+    type UnbundledP dom (t:ts) = Signal dom t : UnbundledP dom ts
+
+    bundleP (x ::> xs) = (::>) <$> x <*> bundleP xs
+    unbundleP s = (headP <$> s) ::> (unbundleP (tailP <$> s))
+
+headP :: Product (t : ts) -> t
+headP (x ::> xs) = x
+
+tailP :: Product (t : ts) -> Product ts
+tailP (x ::> xs) = xs
+
+deriving instance (Show t, Show (Product ts)) => Show (Product (t : ts))
+deriving instance (Generic t, Generic (Product ts)) => Generic (Product (t : ts))
+deriving instance (Generic t, Generic (Product ts), NFData t, NFData (Product ts)) => NFData (Product (t : ts))
+deriving instance (Generic t, Generic (Product ts), Undefined t, Undefined (Product ts)) => Undefined (Product (t : ts))
+
+instance (IsProduct ts) => Bundle (Product ts) where
+    type Unbundled dom (Product ts) = Product (UnbundledP dom ts)
+
+    bundle = bundleP
+    unbundle = unbundleP
