f538c4d4bcf6adec33d0bf228edb1eaaf5d674d4
Dr. ERDI Gergo <gergo@erdi.hu>
2019-04-09 18:49:20 +0800

    Rewrite VGA driver so that the type of VGA modedefs only expose the visible display size and the overall pixel clock rate

--- src-clash/Cactus/Clash/VGA.hs
+++ src-clash/Cactus/Clash/VGA.hs
@@ -1,7 +1,8 @@
 {-# LANGUAGE RecordWildCards, ViewPatterns #-}
 {-# LANGUAGE NumericUnderscores #-}
 {-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE GADTs #-}
+{-# LANGUAGE GADTs, DataKinds, NoStarIsType #-}
+{-# LANGUAGE PartialTypeSignatures #-}
 module Cactus.Clash.VGA where
 
 import Clash.Prelude
@@ -18,27 +19,16 @@ data Polarity
     | Negative
     deriving (Show)
 
-class KnownPolarity (polarity :: Polarity) where
-    toBit :: proxy polarity -> Bool -> Bit
+data VGATiming (pre :: Nat) (pulse :: Nat) (post :: Nat) = VGATiming Polarity
 
-instance KnownPolarity Positive where
-    toBit _ = boolToBit
-
-instance KnownPolarity Negative where
-    toBit _ = negate . boolToBit
-
-data VGATiming = VGATiming
-    { polarity :: Polarity
-    , visibleSize, pre, syncPulse, post :: Nat
-    }
-    deriving (Show)
-
-data VGATimings = VGATimings
-    { pixelClock :: Nat
-    , vgaHorizTiming :: VGATiming
-    , vgaVertTiming :: VGATiming
-    }
-    deriving (Show)
+data VGATimings (pixelClock :: Nat) (w :: Nat) (h :: Nat) where
+    VGATimings
+        :: ( KnownNat fps, KnownNat w, KnownNat h
+          , KnownNat preH, KnownNat pulseH, KnownNat postH
+          , KnownNat preV, KnownNat pulseV, KnownNat postV)
+        => VGATiming preH pulseH postH
+        -> VGATiming preV pulseV postV
+        -> VGATimings (FromHz (fps * (w + preH + pulseH + postH) * (h + preV + pulseV + postV))) w h
 
 data VGADriver dom w h = VGADriver
     { vgaVSync :: Signal dom Bit
@@ -49,70 +39,62 @@ data VGADriver dom w h = VGADriver
     , vgaY :: Signal dom (Maybe (Index h))
     }
 
-data SVGATimings (timings :: VGATimings) where
-    SVGATimings
-      :: ( KnownNat w, KnownNat preH, KnownNat pulseH, KnownNat postH
-        , KnownNat h, KnownNat preV, KnownNat pulseV, KnownNat postV
-        , KnownPolarity polH, KnownPolarity polV
-        )
-      => SVGATimings ('VGATimings ps ('VGATiming polH w preH pulseH postH) ('VGATiming polV h preV pulseV postV))
+vgaCounters
+    :: proxy1 w -> VGATiming preH pulseH postH
+    -> proxy2 h -> VGATiming preV pulseV postV
+    -> Proxy '[ [w, preH, pulseH, postH]
+             , [h, preV, pulseV, postV]
+             ]
+vgaCounters _ _ _ _ = Proxy
+
+toSync :: (HiddenClockReset dom gated synchronous) => Polarity -> Signal dom (Maybe a) -> Signal dom Bit
+toSync polarity = fmap toBit . register False . fmap isJust
+  where
+    toBit = case polarity of
+        Positive -> boolToBit
+        Negative -> negate . boolToBit
 
 vgaDriver
     :: forall timings dom gated synchronous s ps polH w preH pulseH postH polV h preV pulseV postV.
       (HiddenClockReset dom gated synchronous)
     => (dom ~ Dom s ps)
-    => (timings ~ 'VGATimings ps ('VGATiming polH w preH pulseH postH) ('VGATiming polV h preV pulseV postV))
-    => SVGATimings timings
+    => VGATimings ps w h
     -> VGADriver dom w h
-vgaDriver timings@SVGATimings = VGADriver{..}
+vgaDriver timings@(VGATimings horizTime@(VGATiming polarityH) vertTime@(VGATiming polarityV)) = VGADriver{..}
   where
-    counters = Proxy
-        @'[ [w, preH, pulseH, postH]
-          , [h, preV, pulseV, postV]
-          ]
-    horiz :-: vert :-: PNil = counterProdSum counters (pure True)
+    horiz :-: vert :-: PNil = counterProdSum (vgaCounters (Proxy @w) horizTime (Proxy @h) vertTime) (pure True)
 
     unpackVGA :: Sum (Indices '[visible, pre, sync, post]) -> (Maybe (Index visible), Maybe (Index sync))
     unpackVGA (Here x) = (Just x, Nothing)
     unpackVGA (There (There (Here cnt))) = (Nothing, Just cnt)
     unpackVGA _ = (Nothing, Nothing)
 
-    (vgaX, toSync (Proxy @polH) -> vgaHSync) = unbundle $ unpackVGA <$> horiz
-    (vgaY, toSync (Proxy @polV) -> vgaVSync) = unbundle $ unpackVGA <$> vert
+    (vgaX, toSync polarityH -> vgaHSync) = unbundle $ unpackVGA <$> horiz
+    (vgaY, toSync polarityV -> vgaVSync) = unbundle $ unpackVGA <$> vert
 
     vgaEndLine = isFalling False (isJust <$> vgaX)
     vgaEndFrame = isFalling False (isJust <$> vgaY)
 
-toSync
-    :: (KnownPolarity polarity, HiddenClockReset dom gated synchronous)
-    => proxy polarity -> Signal dom (Maybe a) -> Signal dom Bit
-toSync polarity = fmap (toBit polarity) . register False . fmap isJust
-
--- | VGA 640*480@60Hz, 25.175 MHz pixel clock
-vga640x480at60 :: SVGATimings ('VGATimings
-    (FromHz 25_175_000)
-    ('VGATiming Negative 640 16 96 48)
-    ('VGATiming Negative 480 11  2 31))
-vga640x480at60 = SVGATimings
+-- | VGA 640*480@60Hz, 25.152 MHz pixel clock
+vga640x480at60 :: VGATimings _ 640 480
+vga640x480at60 = VGATimings @60
+    (VGATiming @16 @96 @48 Negative)
+    (VGATiming @11 @2  @31 Negative)
 
 -- | VGA 800x600@72Hz, 50 MHz pixel clock
-vga800x600at72 :: SVGATimings ('VGATimings
-    (FromHz 50_000_000)
-    ('VGATiming Positive 800 56 120 64)
-    ('VGATiming Positive 600 37   6 23))
-vga800x600at72 = SVGATimings
+vga800x600at72 :: VGATimings _ 800 600
+vga800x600at72 = VGATimings @72
+    (VGATiming @56 @120 @64 Positive)
+    (VGATiming @37 @6   @23 Positive)
 
 -- | VGA 800x600@60Hz, 40 MHz pixel clock
-vga800x600at60 :: SVGATimings ('VGATimings
-    (FromHz 40_000_000)
-    ('VGATiming Positive 800 40 128 88)
-    ('VGATiming Positive 600  1   4 23))
-vga800x600at60 = SVGATimings
+vga800x600at60 :: VGATimings _ 800 600
+vga800x600at60 = VGATimings @60
+    (VGATiming @40 @128 @88 Positive)
+    (VGATiming @1  @4   @23 Positive)
 
--- | VGA 1024*768@60Hz, 65 MHz pixel clock
--- | VGA 800x600@60Hz, 40 MHz pixel clock
-vga1024x768at60 :: SVGATimings ('VGATimings
-    (FromHz 40_000_000)
-    ('VGATiming Negative 1024 24 136 160)
-    ('VGATiming Negative  768  3   6  29))
-vga1024x768at60 = SVGATimings
+-- | VGA 1024x768@60Hz, 65 MHz pixel clock
+vga1024x768at60 :: VGATimings _ 1024 768
+vga1024x768at60 = VGATimings @60
+    (VGATiming @24 @136 @160 Negative)
+    (VGATiming @3  @6   @29  Negative)
