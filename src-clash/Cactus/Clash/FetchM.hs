184c9ad4a2031873638b0ec544682773abb28127
Dr. ERDI Gergo <gergo@erdi.hu>
2019-06-06 20:08:31 +0800

    Add a monadic interface to fetching from memory

---
+++ src-clash/Cactus/Clash/FetchM.hs
@@ -0,0 +1,48 @@
+{-# LANGUAGE RecordWildCards, GeneralizedNewtypeDeriving #-}
+module Cactus.Clash.FetchM where
+
+import Prelude ()
+import Clash.Prelude hiding (lift)
+
+import Control.Monad.Reader
+import Control.Monad.State
+import Control.Monad.Trans.Except
+import Data.Default
+
+data Failure
+    = Underrun
+    | Overrun
+    deriving Show
+
+data Buffer n dat = Buffer
+    { bufferContents :: Vec n dat
+    , bufferNext :: Index (1 + n)
+    }
+    deriving (Show, Generic, Undefined)
+
+instance (KnownNat n, Default dat) => Default (Buffer n dat) where
+    def = Buffer (pure def) 0
+
+remember :: (KnownNat n) => Buffer n dat -> dat -> Buffer n dat
+remember Buffer{..} x = Buffer
+    { bufferContents = replace bufferNext x bufferContents
+    , bufferNext = bufferNext + 1
+    }
+
+newtype FetchM n dat m a = FetchM{ unFetchM :: ReaderT (Buffer n dat) (StateT (Index (1 + n)) (ExceptT Failure m)) a }
+    deriving newtype (Functor, Applicative, Monad)
+
+runFetchM :: (Monad m, KnownNat n) => Buffer n dat -> FetchM n dat m a -> m (Either Failure a)
+runFetchM buf act = runExceptT $ evalStateT (runReaderT (unFetchM act) buf) 0
+
+fetch :: (Monad m, KnownNat n) => FetchM n dat m dat
+fetch = do
+    Buffer{..} <- FetchM ask
+    idx <- FetchM get
+    when (idx == maxBound) overrun
+    when (idx >= bufferNext) underrun
+    FetchM $ put $ idx + 1
+    return $ bufferContents !! idx
+  where
+    overrun = FetchM . lift . lift . throwE $ Overrun
+    underrun = FetchM . lift . lift . throwE $ Underrun
