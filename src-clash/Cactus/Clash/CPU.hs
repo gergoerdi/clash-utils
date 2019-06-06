62337a268f7d2ac7f11f8198f7930afc6638ae40
Dr. ERDI Gergo <gergo@erdi.hu>
2019-06-06 20:00:15 +0800

    Change representation of CPU monad: instead of collecting an Endo CPUOut, collect a partial overwrite of CPUOut as data (motivated by https://github.com/clash-lang/clash-compiler/issues/613)

--- src-clash/Cactus/Clash/CPU.hs
+++ src-clash/Cactus/Clash/CPU.hs
@@ -1,35 +1,52 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving, DerivingStrategies #-}
-module Cactus.Clash.CPU where
+{-# LANGUAGE DeriveGeneric, UndecidableInstances, MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances, StandaloneDeriving #-}
+module Cactus.Clash.CPU
+       ( CPU
+       , input, output, abort
+       , runCPU, runCPUDebug
+       , HKD(..)
+
+       , unG -- XXX
+       ) where
 
 import Clash.Prelude hiding (lift)
+import Cactus.Clash.CPU.Internal
 import Control.Monad.State hiding (state)
-import Control.Monad.Writer as W
+import GHC.Generics
+import Control.Monad.Identity
 import Control.Monad.RWS
 import Control.Monad.Trans.Class
 import Control.Monad.Trans.Except
-import Data.Monoid
 
-newtype CPU i s o a = CPU{ unCPU :: ExceptT () (RWS i (Endo o) s) a }
-    deriving newtype (Functor, Applicative, Monad, MonadState s)
+newtype CPU i s o a = CPU{ unCPU :: ExceptT () (RWS i (G (o Last)) s) a }
+    deriving newtype (Functor)
+deriving newtype instance (Generic (o Last), GMonoid (Rep (o Last))) => Applicative (CPU i s o)
+deriving newtype instance (Generic (o Last), GMonoid (Rep (o Last))) => Monad (CPU i s o)
+deriving newtype instance (Generic (o Last), GMonoid (Rep (o Last))) => MonadState s (CPU i s o)
 
-tell :: (o -> o) -> CPU i s o ()
-tell = CPU . W.tell . Endo
-
-input :: CPU i s o i
+input :: (Generic (o Last), GMonoid (Rep (o Last))) => CPU i s o i
 input = CPU ask
 
-abort :: CPU i s o a
+output :: (Generic (o Last), GMonoid (Rep (o Last))) => o Last -> CPU i s o ()
+output = CPU . tell . G
+
+abort :: (Generic (o Last), GMonoid (Rep (o Last))) => CPU i s o a
 abort = CPU $ throwE ()
 
-runCPU :: (s -> o) -> CPU i s o () -> (i -> State s o)
+runCPU
+  :: (Generic (o Identity), Generic (o Last), GUpdate (Rep (o Identity)) (Rep (o Last)))
+  => (s -> o Identity) -> CPU i s o () -> (i -> State s (o Identity))
 runCPU mkDef cpu inp = do
     s <- get
-    let (s', f) = execRWS (runExceptT $ unCPU cpu) inp s
+    let (s', writes) = execRWS (runExceptT $ unCPU cpu) inp s
     put s'
     def <- gets mkDef
-    return $ appEndo f def
+    return $ update def (unG writes)
 
-runCPUDebug :: (s -> o) -> CPU i s o () -> (i -> State s (s, o))
+runCPUDebug
+  :: (Generic (o Identity), Generic (o Last), GUpdate (Rep (o Identity)) (Rep (o Last)))
+  => (s -> o Identity) -> CPU i s o () -> (i -> State s (s, o Identity))
 runCPUDebug mkDef cpu inp = do
     s0 <- get
     out <- runCPU mkDef cpu inp
